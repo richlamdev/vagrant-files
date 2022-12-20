155abdac4eb95d3e1326c4f9973d80e1e42bb6a1
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2022-12-19 22:20:33 -0800

    update setup, add vagrant user to wheel group; change root password to simple password for testing
    Co-authored-by: Richard Lam <103448443+procurify-rlam@users.noreply.github.com>

--- freebsd/setup.sh
+++ freebsd/setup.sh
@@ -2,19 +2,23 @@
 
 # remove previous host keys, if any
 ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "192.168.60.50"
-#ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "192.168.13.202"
 
-# set root password to: password123.
-# this is VERY insecure, obviously.  For testing purposes only!!
 #
 # Notes:
 # 1) This is for ephemeral FreeBSD vagrant machines
 # 2) This is not for production
 # 3) Normal users should be provisioned access via SSH key, after python3 is installed on FreeBSD
 # 4) The root user password should be much longer and complex!
-ssh vagrant@192.168.60.50 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'echo -e "password1\npassword1" | sudo passwd root'
-ssh vagrant@192.168.60.50 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'sudo pw usermod vagrant -G wheel'
-#ssh vagrant@192.168.13.202 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'echo -e "password1\npassword1" | sudo passwd root'
-#ssh vagrant@192.168.13.210 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'echo -e "password1\npassword1" | sudo passwd root'
+
+# add vagrant user to the wheel group, to allow for su commands.
+# set root password to: password1
+# this is VERY insecure, obviously.  For testing purposes only!!
+# this is done to emulate a standalone installation of FreeBSD to issue commands with su instead of sudo
+
+ssh vagrant@192.168.60.50 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'sudo pw usermod vagrant -G wheel && echo -e "password1\npassword1" | sudo passwd root'
+
+#uncomment to remove vagrant wheel group
+#ssh vagrant@192.168.60.50 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'sudo pw groupmod wheel -d vagrant'
+echo
 
 exit 0
